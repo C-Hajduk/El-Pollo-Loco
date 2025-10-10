5ddf46b2a69be80bafc8122fe9f5f207a24f77d0
C.Hajduk <christian.hajduk@outlook.de>
2025-10-10 19:09:28 +0200

    add  basic structure

---
+++ models/movable-object.class.js
@@ -0,0 +1,18 @@
+class movableObject {
+  x = 120;
+  y = 250;
+  img;
+  height = 150;
+  width = 100;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  moveRight() {
+    // wenn ich Pfeiltaste nach rechts klicke, dann bewegt sich mein Character auf der x Achse weiter
+  }
+
+  moveLeft() {}
+}
