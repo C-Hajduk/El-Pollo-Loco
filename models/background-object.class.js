fdf355cf5a6ffc8eaa6957a7c6df40ebbeffe7bd
C.Hajduk <christian.hajduk@outlook.de>
2025-10-13 08:49:12 +0200

    add backgroundimg

---
+++ models/background-object.class.js
@@ -0,0 +1,9 @@
+class BackgroundObject extends movableObject {
+  width = 720;
+  height = 400;
+  constructor(imagePath, x) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = 480 - this.height;
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -2,6 +2,12 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0)
+  ];
+  bildIndex = 0;
   canvas;
   ctx;
 
@@ -14,36 +20,25 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(
-      this.character.img,
-      this.character.x,
-      this.character.y,
-      this.character.width,
-      this.character.height
-    );
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(
-        enemy.img,
-        enemy.x,
-        enemy.y,
-        enemy.width,
-        enemy.height
-      );
-    });
-    this.clouds.forEach((cloud) => {
-      this.ctx.drawImage(
-        cloud.img,
-        cloud.x,
-        cloud.y,
-        cloud.width,
-        cloud.height
-      );
-    });
+    this.addObjetcsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjetcsToMap(this.enemies);
+    this.addObjetcsToMap(this.clouds);
 
     // Draw() wird immer wieder aufgerufen
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addObjetcsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
