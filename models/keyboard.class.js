082c69318b2bc2b51f783782a1d3c596641d6073
C.Hajduk <christian.hajduk@outlook.de>
2025-10-14 13:07:01 +0200

    add eventlistener keydown character

---
+++ models/keyboard.class.js
@@ -0,0 +1,8 @@
+class Keyboard {
+  RIGHT = false;
+  LEFT = false;
+  UP = false;
+  DOWN = false;
+  space = false;
+  constructor() {}
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,50 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
 
   console.log("my Character is", world.character);
 }
+
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+
+  if (event.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+  if (event.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+  if (event.keyCode == 38) {
+    keyboard.UP = true;
+  }
+  if (event.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+  if (event.keyCode == 32) {
+    keyboard.space = true;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  console.log(event);
+
+  if (event.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+  if (event.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+  if (event.keyCode == 38) {
+    keyboard.UP = false;
+  }
+  if (event.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+  if (event.keyCode == 32) {
+    keyboard.space = false;
+  }
+});
