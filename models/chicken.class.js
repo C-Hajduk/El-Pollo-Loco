c5d275b0c4f346c0157be74977a7a5c0ebc816f3
C.Hajduk <christian.hajduk@outlook.de>
2025-10-14 16:23:10 +0200

    add Endboss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,11 +1,7 @@
 class Chicken extends movableObject {
   y = 330;
   height = 100;
-  IMAGES_WALKING = [
-    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
-  ];
+  IMAGES_WALKING = ["img/3_enemies_chicken/chicken_normal/1_walk/1_w.png", "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png", "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"];
 
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
@@ -20,10 +16,7 @@ class Chicken extends movableObject {
     this.moveLeft();
 
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
+      this.playAnimation(this.IMAGES_WALKING);
     }, 200);
   }
 }
